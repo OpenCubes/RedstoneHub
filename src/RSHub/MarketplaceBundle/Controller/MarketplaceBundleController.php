c61f0226b7324162bde1893a6188d84fd24a0f90
Vinz243 <vinz243@gmail.com>
2013-08-26 17:43:17 +0200

    Removed acme and added layout+assets

---
+++ src/RSHub/MarketplaceBundle/Controller/MarketplaceBundleController.php
@@ -0,0 +1,16 @@
+<?php
+ 
+// src/Sdz/BlogBundle/Controller/BlogController.php
+ 
+namespace Sdz\BlogBundle\Controller;
+ 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+ 
+class BlogController extends Controller
+{
+  public function indexAction()
+  {
+    return new Response("Hello World !");
+  }
+}
